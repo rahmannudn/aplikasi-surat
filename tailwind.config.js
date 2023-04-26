5296932caf9b30cc79b4cc3672a95d2fbc75778a
rahmannudn <52528984+rahmannudn@users.noreply.github.com>
2023-04-27 06:55:56 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    fontFamily: {
+      robotto: ["Roboto", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
